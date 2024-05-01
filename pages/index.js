b0adc8a7bda9010d8325c41ec7206a0da9f427ee
Arthur Schaefer <arthur.r.schaefer@gmail.com>
2024-05-01 21:27:20 +0000

    remove js101 files merged

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Script from "next/script";
 
 function Home() {
   return <h1>Taila, eu te amo muito! Se tu me amas, manda uma selfie dando uma risadinha.</h1>;
-  window.alert ('My First alert !')
 }
 
 export default Home;
