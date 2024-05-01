9a4833f58bdc01a124e5ccd59ad16592a0bb7030
Arthur Schaefer <arthur.r.schaefer@gmail.com>
2024-05-01 21:19:22 +0000

    alert

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
+import Script from "next/script";
+
 function Home() {
   return <h1>Taila, eu te amo muito! Se tu me amas, manda uma selfie dando uma risadinha.</h1>;
+  window.alert ('My First alert !')
 }
 
 export default Home;
